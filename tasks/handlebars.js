4a943b8392bac5674520e7dde2d2f9684964c9ed
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-01-03 10:28:59 -0500

    handlebars_embed, handlebars_precompile

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -12,28 +12,38 @@ module.exports = function(grunt) {
     var util = require('util'),
         path = require('path');
 
-    grunt.registerMultiTask('handlebars', 'Precompile Handlebars templates', function() {
+    function render(self, compiler, templateFormat) {
         var templates = {};
 
-        grunt.file.expandFiles(this.file.src).forEach(function(each) {
+        grunt.file.expandFiles(self.file.src).forEach(function(each) {
             var templateName = path.basename(each, path.extname(each));
             if (templateName in templates) {
                 grunt.log.error(util.format(
                     'Multiple templates with name "%s" (%s)', templateName, each));
             }
 
-            templates[templateName] = 
-                Handlebars.precompile(grunt.file.read(each)).toString();
+            templates[templateName] = compiler(grunt.file.read(each).toString());
         });
 
-        grunt.file.write(this.target, grunt.utils._.keys(templates).map(function(templateName) {
+        grunt.file.write(self.target, grunt.utils._.keys(templates).map(function(templateName) {
             // grunt underscore library is old. should use `pairs`
 
             return util.format('Ember.TEMPLATES[\'%s\'] = ' +
-                'Ember.Handlebars.template(%s);', templateName,
-                templates[templateName]);
+                templateFormat, templateName, templates[templateName]);
         }).join('\n'));
 
-        this.async()();
+        self.async()();
+    }
+
+    grunt.registerMultiTask('handlebars_embed', 'Precompile Handlebars templates', function() {
+        render(this, function(each) {
+            return each.replace(/"/g, '\\"').replace(/\n/g, ' ');
+        }, 'Ember.Handlebars.compile("%s");');
+    });
+
+    grunt.registerMultiTask('handlebars_precompile', 'Embed Handlebars templates', function() {
+        render(this, function(each) {
+            return Handlebars.precompile(each);
+        }, 'Ember.Handlebars.template(%s);');
     });
 };
